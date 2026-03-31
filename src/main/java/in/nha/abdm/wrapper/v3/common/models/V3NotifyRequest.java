54c5d3280c5c29b8c0d3341636728aea1c9968ce
AdminUser <ravi.starnext@gmail.com>
2026-03-31 17:03:26 +0530

    FIX: Payload structure for HI-Notify in V3 (Pluralize careContexts to List)

--- src/main/java/in/nha/abdm/wrapper/v3/common/models/V3NotifyRequest.java
+++ src/main/java/in/nha/abdm/wrapper/v3/common/models/V3NotifyRequest.java
@@ -21,7 +21,7 @@ public class V3NotifyRequest {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class V3Notification {
-        private V3CareContext careContext;
+        private List<V3CareContext> careContexts;
         private List<String> hiTypes;
         private V3Patient patient;
         private String date;
